f352599ad1b7e713a24662585155ff239b37d0d6
mal-link-a <92664394+mal-link-a@users.noreply.github.com>
2023-11-07 20:00:18 +0800

    Add files via upload

--- main-src.js
+++ main-src.js
@@ -1,15 +1,14 @@
-alert("Чек 0");
-const advantagesCheckBox = document.getElementById("advantages-show");
-alert(advantagesCheckBox);
-var advantagesPanel = document.querySelector('.advantages-panel');
 
+var advantagesCheckBox = document.querySelector(".advantages-button__checkbox");
+var advantagesPanel = document.querySelector(".advantages-panel");
+var advantagesCheckBoxImg = document.querySelector(".img_arrow");
 advantagesCheckBox.addEventListener('change', function () {
     if (this.checked) {
-        //advantagesPanel.classList.add('advantages-panel_open');
-        alert("Чек 1");
+        advantagesPanel.classList.add('advantages-panel_open');
+        advantagesCheckBoxImg.classList.add('img_arrow_opened');
     }
     else {
-       // advantagesPanel.classList.remove('advantages-panel_open');
-       alert("Чек 0");
+       advantagesPanel.classList.remove('advantages-panel_open');
+       advantagesCheckBoxImg.classList.remove('img_arrow_opened');
     }
 });
